6c50f9ca6516ecc8eda56e237d3436ddf8fe2928
RafaS92 <manrvaldez..92@gmail.com>
2020-09-08 21:32:28 -0500

    basic structure header and react router working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Switch>
+          <Route path="/search">
+            <h1>ete</h1>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./Home.css";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
     <div>
       This is home
       <div className="home_header">
-        <div className="home_headerLeft"></div>
+        <div className="home_headerLeft">
+          <Link to="/about">About</Link>
+          <Link to="/store">Store</Link>
+        </div>
 
-        <div className="home_headerLRight"></div>
+        <div className="home_headerLRight">
+          <Link to="/gmail">Gmail</Link>
+          <Link to="/images">Images</Link>
+        </div>
       </div>
       <div className="home_body"></div>
     </div>
