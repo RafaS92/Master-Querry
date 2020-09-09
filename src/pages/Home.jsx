e215087fde05c3e40939fb420c7dc771815cc9de
RafaS92 <manrvaldez..92@gmail.com>
2020-09-08 21:21:21 -0500

    dependencies installed and home component structure created

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      This is home
+      <div className="home_header">
+        <div className="home_headerLeft"></div>
+
+        <div className="home_headerLRight"></div>
+      </div>
+      <div className="home_body"></div>
+    </div>
+  );
+}
+
+export default Home;
