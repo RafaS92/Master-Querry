1a345beeec15633f3b669eab11d7248a050742aa
RafaS92 <manrvaldez..92@gmail.com>
2020-09-10 21:59:39 -0500

    buttons on search working

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
+import { Button } from "@material-ui/core";
 import MicIcon from "@material-ui/icons/Mic";
 
 function Search() {
+  const search = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="search">
       <div className="search_input">
         <SearchIcon className="search_inputIcon" />
         <input />
         <MicIcon />
+      </div>
+
+      <div className="search_buttons">
+        <Button variant="outlined" onClick={search}>
+          I feel dirty
+        </Button>
+        <Button variant="outlined">Google Search</Button>
       </div>
     </div>
   );
