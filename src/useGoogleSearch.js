fef35f090880f3586f411960ebb78694ce536c36
RafaS92 <manrvaldez..92@gmail.com>
2020-09-12 11:25:53 -0500

    apike from google to search available and google search component created

---
+++ src/useGoogleSearch.js
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react'
+
+const CONTEXT_KEY = "8d91816fccd4845a5"
+
+const useGoogleSearch() {
+
+    const [data,setData] = useState(null)
+
+    useEffect(() => {
+
+    },[term])
+   
+}
+
+export default useGoogleSearch
