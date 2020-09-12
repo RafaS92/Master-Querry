e6d29597815c142bc8aa7067d34819214f331827
RafaS92 <manrvaldez..92@gmail.com>
2020-09-12 14:57:42 -0500

    fetching working

--- src/useGoogleSearch.js
+++ src/useGoogleSearch.js
@@ -1,29 +1,27 @@
-import {useState, useEffect} from 'react'
-import { API_KEY } from './keys';
+import { useState, useEffect } from "react";
+import { API_KEY } from "./keys";
 
-const CONTEXT_KEY = "8d91816fccd4845a5"
+const CONTEXT_KEY = "8d91816fccd4845a5";
 
-const useGoogleSearch() {
+const useGoogleSearch = (term) => {
+  const [data, setData] = useState(null);
 
-    const [data,setData] = useState(null)
+  useEffect(() => {
+    const fetchData = async () => {
+      fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
+      )
+        .then((response) => response.json())
+        .then((result) => {
+          setData(result);
+        });
+    };
 
-    useEffect(() => {
-        const fetchData = async() => {
-            fetch(
-                `https://www.googleapis.com/customsearch/v1?keys=${API_KEY}&cx=${CONTEXT_KEY}&q=${term}`
-            )
-            .then(res => res.json())
-            .then(res => {
-                setData(res)
-            })
-        }
+    fetchData();
+  }, [term]);
+  console.log(CONTEXT_KEY);
 
-        fetchData()
+  return { data };
+};
 
-        return {data}
-
-    },[term])
-   
-}
-
-export default useGoogleSearch
+export default useGoogleSearch;
