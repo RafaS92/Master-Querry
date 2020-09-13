d4c3d5110e5285e78c9596f815f305fc1bb82c67
RafaS92 <manrvaldez..92@gmail.com>
2020-09-13 14:35:11 -0500

    graving google queries working

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -2,15 +2,27 @@ import React from "react";
 import "./SearchPage.css";
 import { useStateValue } from "../stateProvider";
 import useGoogleSearch from "../useGoogleSearch";
+import Response from "../response";
+import { Link } from "react-router-dom";
+import Search from "./Search";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
-  const { data } = useGoogleSearch(term);
+  const data = Response;
+
   console.log(data);
   return (
     <div className="searchPage">
       <div className="searchPage_header">
-        <h1>{term}</h1>
+        <Link to="/">
+          <img
+            alt=""
+            src="https://4.bp.blogspot.com/-c0HYC0CsITI/UBDb6v2nE4I/AAAAAAAAEHs/xb6UCx8Ka74/s1600/storm+thunder+lightning+night+sky+photo+pics+animation+gifs+free+download+3D+HD+wallpaper+background+ecards+decoration+graphic+arts+for+websites+amazing+landscape+with+cactuses+and+lightning.gif"
+          />
+        </Link>
+        <div className="searchPage_headerBody">
+          <Search hideButtons />
+        </div>
       </div>
 
       <div className="searchPage_results"></div>
